d5699faa5ca9f83189a837f2a06ef9eca23bafc0
NurullahHarunKose <nurullahharunkose@hotmail.com>
2024-10-10 23:30:56 +0300

    Add files via upload

---
+++ lesson 3/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // lesson 3 (Veriables)
+
+    char myletter='N';
+
+    int mynumber=7;
+
+    float mynumber2=2.56;
+
+    double mynumber3=25.45468448464;
+
+    printf("%c\n",myletter);
+
+    printf("%d\n",mynumber);
+
+    printf("%f\n",mynumber2);
+
+    printf("%lf\n",mynumber3);
+
+    return 0;
+}
