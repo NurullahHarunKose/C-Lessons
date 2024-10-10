1503529a4066c9fdf1098cafa9e82d6f66a7fa23
NurullahHarunKose <nurullahharunkose@hotmail.com>
2024-10-10 23:29:10 +0300

    Add files via upload

---
+++ Lesson 1-2/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // lesson 1 (create a comment line)
+    // this is my comment
+
+    /*
+    machine can't see this
+    this is grat
+    */
+
+    printf("My First C Code\n\n");
+    printf("My Second C Code\n");
+
+    // lesson 2 (escape sequances (kacıs karakterleri))
+
+    printf("my my \b\b\b laptop is great\n");
+    printf("I lake \t\t my laptop\n");
+    printf("my laptop's name is \"Great Laptop\"\n");
+    printf("\\ isareti koymaya yarar\n");
+
+
+    return 0;
+}
