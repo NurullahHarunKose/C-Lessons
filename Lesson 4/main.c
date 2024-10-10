4102dff6d6366dd4e270e0caaa9d967ddd2e3e8e
NurullahHarunKose <nurullahharunkose@hotmail.com>
2024-10-10 23:32:00 +0300

    Add files via upload

---
+++ Lesson 4/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+    int mynumber,mynumber2,mynumber3,sum,extraction,division;
+
+    mynumber=7;
+    mynumber2=10;
+    mynumber3=5;
+
+    sum=mynumber + mynumber2 + mynumber3;
+    extraction=mynumber2 - mynumber3;
+
+    division=mynumber2 / mynumber3;
+
+    printf("sum is: %d\n",sum);
+    printf("extraction is: %d\n",extraction);
+
+    printf("division is: %d\n",division);
+
+
+
+
+
+
+
+
+    return 0;
+}
